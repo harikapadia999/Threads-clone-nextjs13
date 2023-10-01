e7398881a571cbeb67c783ddbd201532e13e04a2
harikapadia999 <harikapadia999@gmail.com>
2023-10-01 16:29:26 +0530

    Resolve error in Onboarding component

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import {
 } from "@/components/ui/form";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
   user: {
@@ -34,6 +36,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -65,9 +68,19 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   };
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    console.log(values);
-  }
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    // console.log(values);
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
+
+    // todo : update user profile
+  };
 
   return (
     <Form {...form}>
