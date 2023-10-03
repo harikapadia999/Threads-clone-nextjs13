13c1463c1b94bc77a3a09e59f3b669badb7cd983
harikapadia999 <harikapadia999@gmail.com>
2023-10-04 00:00:38 +0530

    Create thread content and connect with DB.

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -141,6 +141,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -180,6 +181,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -199,6 +201,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
